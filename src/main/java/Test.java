0b94f518cbac334c9cb2865353bb71ef1997dc4d
Alex S. <syantovich.a.s@gmail.com>
2024-02-13 21:19:05 +0300

    first commit

---
+++ src/main/java/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    static public void main (String[] arg){
+        byte a = 12;
+        byte b = 14;
+        int c = a + b;
+
+        System.out.println("c = " + c);
+    }
+}
