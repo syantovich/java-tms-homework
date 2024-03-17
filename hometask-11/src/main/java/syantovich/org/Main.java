6d3a293c6ff286b952a5e8e31356bc24b86cc0b4
Alex S. <syantovich.a.s@gmail.com>
2024-03-17 23:47:44 +0300

    home task 11 cr

--- hometask-11/src/main/java/syantovich/org/Main.java
+++ hometask-11/src/main/java/syantovich/org/Main.java
@@ -13,5 +13,25 @@ public class Main {
         System.out.println(string.substring(10));
 
         System.out.println(new StringBuilder(string).reverse());
+
+        public class User {
+            String name;
+            int age;
+            double salary;
+
+            User(String name, int age, double salary) {
+                this.name = name;
+                this.age = age;
+                this.salary = salary;
+            }
+
+            @Override
+            public String toString() {
+                String userInfoFormat = "My name is %s . I'm %d years old. And my current salary is %d";
+                return String.format(userInfoFormat, this.name, this.age, this.salary);
+            }
+        }
+
+        System.out.println(new User('Andrei', 48, 90000));
     }
 }
